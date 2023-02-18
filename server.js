8c014832125879c528acfc863abac5f8094a12b2
James McGill <jmcgill@plexer.net>
2023-02-18 09:19:07 -0500

    Purge cache

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const stream = require('stream');
 app.get('/test', async (req, res) => {
     res.send('OK TEST');
 
-}
+});
+
 app.get('/download', async (req, res) => {
     console.log('This is something');
     // const dataUrl = req.params.dataUrl;
