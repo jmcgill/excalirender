560ef47b223d3481e66e09dd86ef35ba33f88adf
James McGill <jmcgill@plexer.net>
2023-02-18 10:46:08 -0500

    Specify port in environment variable

--- server.js
+++ server.js
@@ -15,35 +15,35 @@ app.get('/download', async (req, res) => {
     // const dataUrl = req.params.dataUrl;
     const browser = await puppeteer.launch({ args: ['--no-sandbox', "--disable-setuid-sandbox"], ignoreHTTPSErrors: true });
     const page = await browser.newPage();
-    await page.goto('https://plexer.net');
+    await page.goto(path.join(__dirname, 'index.html'));
 
-    await page.waitForSelector('.big-title')
-    const element = await page.$('.big-title')
-    const value = await page.evaluate(el => el.textContent, element);
+    // await page.waitForSelector('.big-title')
+    // const element = await page.$('.big-title')
+    // const value = await page.evaluate(el => el.textContent, element);
+    //
+    // res.send(value);
 
-    res.send(value);
+    await page.waitForSelector('#render_button');
+    await page.click('#render_button');
 
-    // await page.goto('file:///Users/jmcgill/src/excalirender/index.html');
-    // await page.click('#render_button');
-    //
-    // let element = await page.$('#img');
-    // let value = await element.evaluate(el => el.textContent);
-    //
-    // browser.close();
-    //
-    // const matches = value.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-    // // const imageExtension = matches[1].split('/')[1];
-    // const imageData = matches[2];
-    // const buffer = Buffer.from(imageData, 'base64');
-    // // const filename = `${Date.now()}.${imageExtension}`;
-    // // var fileContents = Buffer.from(fileData, "base64");
-    //
-    // var readStream = new stream.PassThrough();
-    // readStream.end(buffer);
-    //
-    // // response.set('Content-disposition', 'attachment; filename=' + fileName);
-    // res.set('Content-Type', 'image/x-png');
-    // readStream.pipe(res);
+    let element = await page.$('#img');
+    let value = await element.evaluate(el => el.textContent);
+
+    browser.close();
+
+    const matches = value.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    // const imageExtension = matches[1].split('/')[1];
+    const imageData = matches[2];
+    const buffer = Buffer.from(imageData, 'base64');
+    // const filename = `${Date.now()}.${imageExtension}`;
+    // var fileContents = Buffer.from(fileData, "base64");
+
+    var readStream = new stream.PassThrough();
+    readStream.end(buffer);
+
+    // response.set('Content-disposition', 'attachment; filename=' + fileName);
+    res.set('Content-Type', 'image/x-png');
+    readStream.pipe(res);
 });
 
 app.listen(process.env.PORT, () => {
