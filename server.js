10e60373fa90aab65cf9ce939db8c87a38385b4b
James McGill <jmcgill@plexer.net>
2023-02-17 21:57:07 -0500

    Specify port in environment variable

--- server.js
+++ server.js
@@ -2,34 +2,35 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
 const stream = require('stream');
 
 app.get('/download', async (req, res) => {
-    // const dataUrl = req.params.dataUrl;
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('file:///Users/jmcgill/src/excalirender/index.html');
-    await page.click('#render_button');
-
-    let element = await page.$('#img');
-    let value = await element.evaluate(el => el.textContent);
-
-    browser.close();
-
-    const matches = value.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-    // const imageExtension = matches[1].split('/')[1];
-    const imageData = matches[2];
-    const buffer = Buffer.from(imageData, 'base64');
-    // const filename = `${Date.now()}.${imageExtension}`;
-    // var fileContents = Buffer.from(fileData, "base64");
-
-    var readStream = new stream.PassThrough();
-    readStream.end(buffer);
-
-    // response.set('Content-disposition', 'attachment; filename=' + fileName);
-    res.set('Content-Type', 'image/x-png');
-    readStream.pipe(res);
+    res.send('OK');
+    // // const dataUrl = req.params.dataUrl;
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
+    // await page.goto('file:///Users/jmcgill/src/excalirender/index.html');
+    // await page.click('#render_button');
+    //
+    // let element = await page.$('#img');
+    // let value = await element.evaluate(el => el.textContent);
+    //
+    // browser.close();
+    //
+    // const matches = value.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    // // const imageExtension = matches[1].split('/')[1];
+    // const imageData = matches[2];
+    // const buffer = Buffer.from(imageData, 'base64');
+    // // const filename = `${Date.now()}.${imageExtension}`;
+    // // var fileContents = Buffer.from(fileData, "base64");
+    //
+    // var readStream = new stream.PassThrough();
+    // readStream.end(buffer);
+    //
+    // // response.set('Content-disposition', 'attachment; filename=' + fileName);
+    // res.set('Content-Type', 'image/x-png');
+    // readStream.pipe(res);
 });
 
 app.listen(process.env.PORT, () => {
