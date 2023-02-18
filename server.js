aefec1322346072c88791eafc5060b2088316bab
James McGill <jmcgill@plexer.net>
2023-02-18 10:43:10 -0500

    Specify port in environment variable

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/test', async (req, res) => {
 app.get('/download', async (req, res) => {
     console.log('This is something');
     // const dataUrl = req.params.dataUrl;
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', "--disable-setuid-sandbox"] });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', "--disable-setuid-sandbox"], ignoreHTTPSErrors: true });
     const page = await browser.newPage();
     await page.goto('https://plexer.net');
 
